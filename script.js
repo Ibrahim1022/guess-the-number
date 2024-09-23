7455e5074bd8515b16914bc19ed2758444e19b38
Ibrahim1022 <ibrahimhusseinjama1995@gmail.com>
2024-09-23 10:39:38 +0300

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 //code
 // gets a random number from 1 - 20
 let randomNumber = Math.floor(Math.random() * 20) + 1
-let score = 20
-let highScore = 0
+console.log(Math.trunc(Math.random()* 20) +1)
+document.querySelector(".check").addEventListener("click", function(){
+
+// Get users input
+const input = document.querySelector(".guess").value
+console.log(input);
+document.querySelector(".number").textContent = randomNumber;
+if(input === randomNumber){
+    document.querySelector("body").style.backgroundColor = "green";
+    console.log("Correct!");
+}else{
+    document.querySelector(".message").textContent = "Wrong Number!"; 
+} 
+})
+
+// background changes to green
+// message should be correct number
+// should display random number
